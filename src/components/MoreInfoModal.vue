ffa76e6b093143fdfa982a419305ebcde8aac328
Kevin <nelson67@stolaf.edu>
2019-09-01 03:25:20 -0500

    add better looking modal

--- src/components/MoreInfoModal.vue
+++ src/components/MoreInfoModal.vue
@@ -1,30 +1,31 @@
 <template>
-  <div id="modal-container">
-    <modal :name="modalName"
+  <div>
+    <modal name="more-info"
            width="80%"
            height="auto"
+           style="margin-bottom: 0px;"
     >
-      <div id="info-container">
-        <div class="spacing">
-          <h5 id="name">
-            {{ moreInfoData.name }}
-          </h5>
-        </div>
-        <div class="spacing">
-          <h5 class="modal-label">Prereqs</h5>
-            {{ moreInfoData.prereqs }}
-        </div>
-        <div class="spacing">
-          <h5 class="modal-label">Description</h5>
-          {{ moreInfoData.description }}
-        </div>
+        <div id="info-container">
+          <div class="spacing">
+            <h5 id="name">
+              {{ moreInfoData.name }}
+            </h5>
+          </div>
+          <div class="spacing">
+            <h5 class="modal-label">Prereqs</h5>
+              {{ moreInfoData.prereqs }}
+          </div>
+          <div class="spacing">
+            <h5 class="modal-label">Description</h5>
+            {{ moreInfoData.description }}
+          </div>
 
-        <div class="spacing">
-          <h5 class="modal-label">Notes</h5>
-          {{ moreInfoData.notes }}
+          <div class="spacing">
+            <h5 class="modal-label">Notes</h5>
+            {{ moreInfoData.notes }}
+          </div>
         </div>
-      </div>
-      <b-button id="modal-close-btn">Close</b-button>
+        <!-- <button @click="close" id="modal-close-btn">Close</button> -->
     </modal>
   </div>
 </template>
@@ -32,7 +33,12 @@
 <script>
 export default {
   name: 'more-info-modal',
-  props: ['moreInfoData', 'modalName']
+  props: ['moreInfoData', 'modalName'],
+  methods: {
+    close() {
+      this.$modal.hide('more-info')
+    }
+  }
 }
 </script>
 
@@ -46,14 +52,21 @@ export default {
 }
 
 .modal-label {
+  color: #ccc;
   font-size: 20px;
   font-weight: 900;
 }
 
 #modal-close-btn {
-  border-width: 3px;
+  font-size: 20px;
+  color: #ddd;
+  border: none;
+  margin: 0px;
+  /*border: solid blue;*/
+  /*border-width: 3px;*/
   height: 50px;
   width: 100%;
+  background: #2c3e50;
 }
 
 .spacing {
@@ -66,8 +79,9 @@ export default {
 }
 
 #info-container {
+  color: #eee;
+  background: url(https://wallpapercave.com/wp/wp4078020.jpg) fixed;
   padding: 30px;
-  /*background: #167df0;*/
 }
 
 </style>
